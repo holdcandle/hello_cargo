18d3eccfd307aa120f7559e7f6934b9bf87fe09d
holdcandle <2598692927@qq.com>
2025-09-26 17:22:18 +0800

    feat(src): 添加猜数字游戏功能
    新增了一个简单的猜数字游戏，程序会提示用户输入猜测的数字，并读取用户输入的内容。这是对原有 hello world 程序的扩展，增加了用户交互功能。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
